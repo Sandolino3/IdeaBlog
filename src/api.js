9227da5f2304bcff07997f854799dc2b558f0638
Sandolino3 <89142522+Sandolino3@users.noreply.github.com>
2025-03-03 12:59:01 +0200

    Restored mistake

---
+++ src/api.js
@@ -0,0 +1,54 @@
+const host = "https://localhost:3030"
+
+export async function request(method, url, data) {
+    const options = {
+        method,
+        headers: {
+
+        },
+    }
+
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+        const token = user.accessToken
+        options.headers['X-Authorization'] = token
+    }
+
+    if (data != undefined) {
+        options.headers['Content-Type'] = 'application/json'
+        options.body = JSON.stringify(data)
+    }
+
+    try {
+        const response = await fetch(host + url, options)
+        if (response.ok != true) {
+            if (response.status == 403) {
+                localStorage.removeItem('user')
+            }
+            const error = await response.json()
+            throw new Error(error.message);
+        }
+        if (response.status == 204) {
+            return response
+        } else {
+            return response.json()
+        }
+
+    } catch (err) {
+        alert(err.massage)
+        throw err
+    }
+}
+
+
+const get = request.bind(null, 'GET');
+const post = request.bind(null, 'POST');
+const put = request.bind(null, 'PUT');
+const del = request.bind(null, 'DELETE');
+
+export{
+    get,
+    post,
+    put,
+    del as delete
+}
---
+++ src/users.js
@@ -0,0 +1,28 @@
+import { get, post } from "./api.js";
+
+const endpoints = {
+    'login': '/users/login',
+    'register': '/users/register',
+    'logout': '/users/logout'
+}
+
+export async function register(email, password) {
+    const user = post(endpoints.register, {email, password})
+
+    localStorage.setItem('user', JSON.stringify(user))
+
+}
+
+export async function login(email, password){
+    const user = post(endpoints.login, {email,password})
+
+    localStorage.setItem('user', JSON.stringify(user))
+
+}
+
+export async function logout(){
+    get(endpoints.logout)
+    localStorage.removeItem('user')
+
+}
+
